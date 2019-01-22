0064f79307c5e3d2e6d3ad1ec38a29f11e30e4be
wyy <610193653@qq.com>
2019-01-23 00:30:56 +0800

    conserve changes

--- lunbotu_lianxiban/lunbotu.js
+++ lunbotu_lianxiban/lunbotu.js
@@ -1,3 +1,15 @@
 <script type="text/javascript">
- 
+  var wrap = document.querySelector(".wrap");
+  var next = document.querySelector(".arrow_right");
+  var prev = document.querySelector("arrow_left");
+  function next_pic (){
+  	var newLeft = parseInt(wrap.style.left)-600;
+  	wrap.style.left = newLeft + "px";
+  }
+  next.onclick = function (){
+  	next_pic()
+  }
+  prev.onclick = function(){
+  	prev_pic()
+  }
 </script>
