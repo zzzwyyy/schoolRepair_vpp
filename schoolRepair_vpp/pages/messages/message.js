1b899cde7b8a8439cd0686170f7383c516798b3b
wyy <610193653@qq.com>
2019-03-20 19:59:22 +0800

    changes

--- schoolRepair_vpp/pages/messages/message.js
+++ schoolRepair_vpp/pages/messages/message.js
@@ -1,15 +1,37 @@
+App = getApp();
 Page({
   data: {
-    sex: '',
-    managerName: '',
-    managerId: '',
-    managerPhone: '',
+    items:[
+      { name: "图书馆", value: "图书馆" },
+      { name: "宿舍", value: "宿舍" },
+      { name: "教室", value: "教室" },
+      { name: "体育馆", value: "体育馆" },
+      { name: "图书馆", value: "图书馆" },
+      { name: "游泳馆", value: "游泳馆" }
+    ],
+    show:false
   },
  
+  checkboxChange:function(e){
+      console.log(e.detail.value)
+  },
 
   formSubmit:function(e) {
-    let { managerName,sex, managerId, managerPhone } = e.detail.value;
-    console.log(e.detail.value);
+    let { managerName, sex, managerId, managerPhone } = e.detail.value;
+    if (!managerName || !managerId || !managerPhone){
+      this.setData({
+         show:true
+      })
+    }
+    else{
+      this.setData({
+         managerName,
+         sex,
+         managerId,
+         managerPhone 
+      })
+
+    }
   
   }
 })
