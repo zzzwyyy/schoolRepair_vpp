416c2cc7e546d0f17bb265b4e25c2712348e10dc
wyy <610193653@qq.com>
2019-03-23 11:53:00 +0800

    changes

--- schoolRepair_vpp/pages/shouye/shouye.js
+++ schoolRepair_vpp/pages/shouye/shouye.js
@@ -1,72 +1,32 @@
 Page({
   data: {
-    items:[
-      "宿舍",
-      "教学楼",
-      "图书馆",
-    ],
-    floors:[
-      "一楼",
-      "二楼",
-      "三楼",
-      "四楼",
-      "五楼",
-    ],
+    selected1: true,
+    selected2: false,
+    selected3: false,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  selected1:function(e){
+    this.setData({
+      selected1: true,
+      selected2: false,
+      selected3: false,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  selected2: function (e) {
+    this.setData({
+      selected1: false,
+      selected2: true,
+      selected3: false,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  selected3: function (e) {
+    this.setData({
+      selected1: false,
+      selected2: false,
+      selected3: true,
+    })
   }
+
 })
