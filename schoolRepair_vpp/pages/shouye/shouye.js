52ea4b2de45716a89787e55467ecdb88d0794348
wyy <610193653@qq.com>
2019-03-25 23:07:02 +0800

    changes

--- schoolRepair_vpp/pages/shouye/shouye.js
+++ schoolRepair_vpp/pages/shouye/shouye.js
@@ -1,32 +1,81 @@
+var app = getApp();
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    selected1: true,
-    selected2: false,
-    selected3: false,
+    items:[
+      '宿舍',
+      '教学楼',
+      '图书馆'
+    ],
+    currentTabIndex:0,
   },
 
-  selected1:function(e){
-    this.setData({
-      selected1: true,
-      selected2: false,
-      selected3: false,
-    })
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
   },
 
-  selected2: function (e) {
-    this.setData({
-      selected1: false,
-      selected2: true,
-      selected3: false,
-    })
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
   },
 
-  selected3: function (e) {
-    this.setData({
-      selected1: false,
-      selected2: false,
-      selected3: true,
-    })
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+  /*点击管理地点的选项卡事件*/
+  currentTap:function(e) {
+     console.log(e.currentTarget.dataset.idx)
+     if(this.data.currentTabINdex == e.currentTarget.dataset.idx) {
+       return;
+     }
+     this.setData({
+       currentTabIndex:e.currentTarget.dataset.idx
+     })
   }
-
 })
